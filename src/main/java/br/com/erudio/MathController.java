0497bab03d6922d9d508946c4fc16752919e2853
Gabriel Porto <gabrielqporto@hotmail.com>
2023-09-11 17:43:50 -0300

    MathController

---
+++ src/main/java/br/com/erudio/MathController.java
@@ -0,0 +1,19 @@
+package br.com.erudio;
+
+import org.springframework.web.bind.annotation.*;
+
+import java.util.concurrent.atomic.AtomicLong;
+
+@RestController
+public class MathController {
+
+    private static final String template = "Hello, %s!";
+    private final AtomicLong counter = new AtomicLong();
+
+    @RequestMapping(value = "/sum/{numberOne}/{numberTwo}", method= RequestMethod.GET)
+    public Double sum(
+            @PathVariable(value = "numberOne") String numberOne,
+            @PathVariable(value = "numberTwo") String numberTwo) {
+        return 1D;
+    }
+}
