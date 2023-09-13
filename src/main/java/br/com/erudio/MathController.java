d3fa2b22f2f2ed719d9e16b580de91906717803c
Gabriel Porto <gabrielqporto@hotmail.com>
2023-09-12 21:36:53 -0300

    calculadora e tratamento de excecoes

--- src/main/java/br/com/erudio/MathController.java
+++ src/main/java/br/com/erudio/MathController.java
@@ -1,5 +1,6 @@
 package br.com.erudio;
 
+import br.com.erudio.exceptions.UnsuportedMathOperationException;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.concurrent.atomic.AtomicLong;
@@ -13,7 +14,24 @@ public class MathController {
     @RequestMapping(value = "/sum/{numberOne}/{numberTwo}", method= RequestMethod.GET)
     public Double sum(
             @PathVariable(value = "numberOne") String numberOne,
-            @PathVariable(value = "numberTwo") String numberTwo) {
-        return 1D;
+            @PathVariable(value = "numberTwo") String numberTwo
+    ) throws Exception {
+        if(!isNumeric(numberOne) || !isNumeric(numberTwo)) {
+            throw new UnsuportedMathOperationException("Please set a numeric value!");
+        }
+        return convertToDouble(numberOne) + convertToDouble(numberTwo);
+    }
+
+    private Double convertToDouble(String strNumber) {
+        if(strNumber == null) return 0D;
+        String number = strNumber.replaceAll(",", ".");
+        if(isNumeric(number)) return Double.parseDouble(number);
+        return 0D;
+    }
+
+    private boolean isNumeric(String strNumber) {
+        if(strNumber == null) return false;
+        String number = strNumber.replaceAll(",", ".");
+        return number.matches("[-+]?[0-9]*\\.?[0-9]+");
     }
 }
