72485c1af4eff9268fbfb9280bc6760c0d38b0e4
Gabriel Porto <gabrielqporto@hotmail.com>
2023-10-21 21:26:04 -0300

    entidade e repositorio

--- src/main/java/br/com/erudio/model/Person.java
+++ src/main/java/br/com/erudio/model/Person.java
@@ -1,22 +1,28 @@
 package br.com.erudio.model;
 
 import br.com.erudio.controllers.PersonController;
+import jakarta.persistence.*;
 
 import java.io.Serializable;
 import java.util.Objects;
-import jakarta.persistence.Table;
 
 @Entity
+@Table(name = "person")
 public class Person implements Serializable {
 
     private static final long serialVersionUID = 1L;
 
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
+    @Column(name = "first_name", nullable = false, length = 80)
     private String firstName;
+    @Column(name = "last_name", nullable = false, length = 80)
     private String lastName;
+    @Column(nullable = false, length = 100)
     private String address;
+    @Column(nullable = false, length = 6)
     private String gender;
-
 
     public Long getId() {
         return id;
